d6463b432ea787b0e8f04b5b8360ede5c92fd017
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-24 19:14:45 +0100

    fix error

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,9 @@
-//IMPORTIAMO COMPONENTI
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
+
 
 export default function HomePage() {
     return (
         <>
-            <Header />
             <h2>Ciao sono la home page</h2>
-            <NavBar />
         </>
 
     );
